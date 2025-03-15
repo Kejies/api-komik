9def2059a75c0b2fb9e326d7f017de4ab0081945
Abyan Shafly Arief <kejies0191@gmail.com>
2025-03-15 10:04:09 +0700

    update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from api_routes import api_routes  # Pastikan ada ini
+from api_routes import api_routes
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(api_routes)
 
 if __name__ == "__main__":
