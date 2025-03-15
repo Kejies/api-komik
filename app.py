4ed9811221001cff8c441be6741f68074beeee45
Abyan Shafly Arief <kejies0191@gmail.com>
2025-03-15 09:43:13 +0700

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from api_routes import api_routes  # Pastikan ada ini
 
 app = Flask(__name__)
-app.register_blueprint(api_routes, url_prefix='/api')
+app.register_blueprint(api_routes)
 
 if __name__ == "__main__":
     app.run()
