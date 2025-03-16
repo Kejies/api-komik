69109d5754c1b0597928cccc61cf88a06a648cf1
kyji <98704712+Kejies@users.noreply.github.com>
2025-03-16 07:00:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-app.register_blueprint(api_routes)
 
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/api')
+def api_docs():
+    return render_template('index.html')
+
+app.register_blueprint(api_routes)
 
 if __name__ == "__main__":
     app.run(debug=True)
