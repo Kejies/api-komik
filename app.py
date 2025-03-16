0f4ec4a87e12046f636de670919bbf9ab9ed1e44
kyji <98704712+Kejies@users.noreply.github.com>
2025-03-16 07:57:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from api_routes import api_routes
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates") 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
