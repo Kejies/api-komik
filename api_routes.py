067edd557efec732db755d329014d4e0b3023fa2
kyji <98704712+Kejies@users.noreply.github.com>
2025-04-09 08:29:05 +0700

    Update api_routes.py

--- api_routes.py
+++ api_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, jsonify, Response
 import json
 from scraper import terbaru, popular, detail, content, search, find_genre, get_manhua_list, get_search_manhua, get_manhua_detail, get_manhua_content
-from scraper_2 import getAnimeListTerbaru, getAnimeDetail, nontonAnime, search_anime, get_anime_popular
 
 api_routes = Blueprint("api_routes", __name__)
 
@@ -79,29 +78,13 @@ def api_content(link):
 
 @api_routes.route('/api/search/<path:query>', methods=['GET'])
 def api_search(query):
-    try:
-        komik_data = search(query)
-    except Exception as e:
-        komik_data = []
-    
-    try:
-        manhua_data = get_search_manhua(query)
-    except Exception as e:
-        manhua_data = []
-    
-    try:
-        anime_data = search_anime(query)
-    except Exception as e:
-        anime_data = []
+    komik_data = search(query)
+    manhua_data = get_search_manhua(query)
 
     data = {
         "success": True,
         "message": "Berhasil mengambil data",
-        "data": {
-            "komik": komik_data,
-            "manhua": manhua_data,
-            "anime": anime_data
-        }
+        "data": [komik_data, manhua_data]
     }
     
     return Response(json.dumps(data, ensure_ascii=False, indent=4), mimetype="application/json")
@@ -132,54 +115,4 @@ def api_get_manhua(page=1):
         "total_pages": str(total_pages),
         "data": komik_data 
     }
-    return Response(json.dumps(data, ensure_ascii=False, indent=4), mimetype="application/json")
-
-@api_routes.route('/api/anime-terbaru/<int:page>', methods=['GET'])
-def api_anime_terbaru(page):
-    anime_data = getAnimeListTerbaru(page)
-
-    data = {
-        "success": True,
-        "message": "Berhasil mengambil data",
-        "current_page": str(page),
-        "total_pages": str(25),
-        "data": anime_data 
-    }
-    
-    return Response(jsonify(data).data, mimetype="application/json")
-
-@api_routes.route('/api/anime-detail/<path:link>', methods=['GET'])
-def api_anime_detail(link):
-    anime_data = getAnimeDetail(link)
-
-    data = {
-        "success": True,
-        "message": "Berhasil mengambil data",
-        "data": anime_data
-    }
-    
-    return Response(json.dumps(data, ensure_ascii=False, indent=4), mimetype="application/json")
-
-@api_routes.route('/api/nontonanime/<path:link>', methods=['GET'])
-def api_nonton_anime(link):
-    anime_data = nontonAnime(link)
-
-    data = {
-        "success": True,
-        "message": "Berhasil mengambil data",
-        "data": anime_data
-    }
-    
-    return Response(json.dumps(data, ensure_ascii=False, indent=4), mimetype="application/json")
-
-@api_routes.route('/api/anime-popular/', methods=['GET'])
-def api_anime_popular():
-    anime_data = get_anime_popular()
-
-    data = {
-        "success": True,
-        "message": "Berhasil mengambil data",
-        "data": anime_data
-    }
-    
     return Response(json.dumps(data, ensure_ascii=False, indent=4), mimetype="application/json")
